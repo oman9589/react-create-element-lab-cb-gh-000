5481278f3a389e9e97462279629b60d8211fc160
Oliver Spring <oliver9589@gmail.com>
2017-09-16 20:20:18 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+const meInReact =
+React.createElement('div', {},
+  React.createElement('h1', {}, 'test'));
+
 ReactDOM.render(
-  meInReact, 
+  meInReact,
   document.getElementById('root')
 );
 
 export default meInReact
-*/
