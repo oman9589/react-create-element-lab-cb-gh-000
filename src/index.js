a51d540bf58a8ee4f8bef5536fac1133afff2b4d
Oliver Spring <oliver9589@gmail.com>
2017-09-16 20:35:00 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const meInReact =
-React.createElement('h1', {}, 'test');
+React.createElement('div', {},
+    React.createElement('h1', {}, 'My favorite ice cream flavors'),
+    React.createElement('ul', {},
+      [
+        React.createElement('li', {}, 'Chocolate'),
+        React.createElement('li', {}, 'Vanilla'),
+        React.createElement('li', {}, 'Banana')
+      ]
+    )
+  );
 
 ReactDOM.render(
   meInReact,
