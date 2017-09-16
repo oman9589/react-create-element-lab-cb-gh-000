d6d164004d3f7da2ce876f7fa794eb539e81a319
Oliver Spring <oliver9589@gmail.com>
2017-09-16 20:27:52 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
+lsimport React from 'react';
 import ReactDOM from 'react-dom';
 
 const meInReact =
-React.createElement('div', {},
-  React.createElement('h1', {}, 'test'));
+  React.createElement('h1', {}, 'test');
 
 ReactDOM.render(
   meInReact,
